b848983e75a986dcd1e51b6563f66d9a5d8693ee
ElkYTHN <elkinportillo4@gmail.com>
2022-07-22 23:32:26 -0600

    Cambio 1

--- app/src/main/java/com/grupom/carwash_proyecto/Modelo/Usuario.java
+++ app/src/main/java/com/grupom/carwash_proyecto/Modelo/Usuario.java
@@ -1,4 +1,150 @@
 package com.grupom.carwash_proyecto.Modelo;
 
 public class Usuario {
+
+    public int id;
+    public String nombres;
+    public String apellidos;
+    public String fechaNac;
+    public String pais;
+    public int codigo_pais;
+    public String correo;
+    public String contrasena;
+    public String Latitud;
+    public String Longitud;
+    public String foto;
+    public String telefono;
+
+    public Usuario(int codigo_usuario, String nombres, String apellidos,String foto) {
+        this.nombres = nombres;
+        this.id = codigo_usuario;
+        this.apellidos = apellidos;
+        this.foto = foto;
+
+    }
+
+    public Usuario(int id, String nombres, String apellidos, String fechaNac, String pais, int codigo_pais, String correo, String foto, String telefono) {
+        this.id = id;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.fechaNac = fechaNac;
+        this.pais = pais;
+        this.codigo_pais = codigo_pais;
+        this.correo = correo;
+        this.foto = foto;
+        this.telefono = telefono;
+    }
+
+    public Usuario(int id, String nombres, String apellidos, String correo,String contrasena, String latitud, String longitud, String foto) {
+        this.id = id;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.contrasena = contrasena;
+        Latitud = latitud;
+        Longitud = longitud;
+        this.foto = foto;
+    }
+
+    public Usuario(String correo, String contrasena) {
+        this.correo = correo;
+        this.contrasena = contrasena;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(String fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public int getCodigo_pais() {
+        return codigo_pais;
+    }
+
+    public void setCodigo_pais(int codigo_pais) {
+        this.codigo_pais = codigo_pais;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getLatitud() {
+        return Latitud;
+    }
+
+    public void setLatitud(String latitud) {
+        Latitud = latitud;
+    }
+
+    public String getLongitud() {
+        return Longitud;
+    }
+
+    public void setLongitud(String longitud) {
+        Longitud = longitud;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
