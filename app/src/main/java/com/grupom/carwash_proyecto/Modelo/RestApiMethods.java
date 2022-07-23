828143715610c029d61cf097cd9bce70a87b3519
ElkYTHN <elkinportillo4@gmail.com>
2022-07-23 17:00:00 -0600

    Cambio 1

--- app/src/main/java/com/grupom/carwash_proyecto/Modelo/RestApiMethods.java
+++ app/src/main/java/com/grupom/carwash_proyecto/Modelo/RestApiMethods.java
@@ -2,4 +2,28 @@ package com.grupom.carwash_proyecto.Modelo;
 
 public class RestApiMethods {
 
+    private static final String ipaddress = "educationsofthn.tech";
+    public static final String StringHttp = "https://";
+
+    private static final String GetBuscar = "/API/validarLogin.php";
+    private static final String getBuscarCorreo = "/API/listasingleusuario.php";
+    private static final String setUpdate = "/API/actualizarusuario.php";
+    private static final String CreateUsuario = "/API/crearusuario.php";
+    private static final String listaPaises = "/API/listapaises.php";
+    private static final String listaUsuarioPaise = "/API/listasingleusuariopais.php";
+
+    public static final String EndPointValidarLogin = StringHttp + ipaddress + GetBuscar;
+    public static final String EndPointBuscarCorreo = StringHttp + ipaddress + getBuscarCorreo;
+    public static final String EndPointSetUpdateUser= StringHttp + ipaddress + setUpdate;
+    public static final String EndPointCreateUsuario = StringHttp + ipaddress + CreateUsuario;
+    public static final String EndPointListarPaises = StringHttp + ipaddress + listaPaises;
+    public static final String EndPointListarUsuarioPaise = StringHttp + ipaddress + listaUsuarioPaise;
+
+    public static String EndPointBuscarUsuario;
+
+    public static  String correo = "";
+    public static  String codigo_usuario = "";
+
+    String prueba = "https://educationsofthn.tech/API/listasingleusuariopais.php";
+
 }
